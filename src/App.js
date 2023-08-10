cbbd2cc884adddc27737c77123d4917187239fed
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-11 03:22:01 +0530

    Initial Structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold bg-fuchsia-600 text-white ">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
